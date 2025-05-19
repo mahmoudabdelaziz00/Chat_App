0e675ffd4b922b71f7b752df6c2367ca241a7267
Mahmoud Abdelaziz <mahmoudabdelaziz00@gmail.com>
2025-05-19 22:31:12 +0300

    add BlocListener widget to login screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
+import 'package:chat_app/cubits/login_cubit/login_cubit.dart';
 import 'package:chat_app/screens/chat_screen.dart';
 import 'package:chat_app/screens/login_screen.dart';
 import 'package:chat_app/screens/register_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'firebase_options.dart';
 
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
@@ -19,14 +21,17 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      routes: {
-        LoginScreen.id:(context)=>LoginScreen(),
-        RegisterScreen.id:(context)=>RegisterScreen(),
-        ChatScreen.id:(context)=>ChatScreen(),
-      },
-      initialRoute: LoginScreen.id,
+    return BlocProvider(
+      create: (context) => LoginCubit(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        routes: {
+          LoginScreen.id: (context) => LoginScreen(),
+          RegisterScreen.id: (context) => RegisterScreen(),
+          ChatScreen.id: (context) => ChatScreen(),
+        },
+        initialRoute: LoginScreen.id,
+      ),
     );
   }
 }
