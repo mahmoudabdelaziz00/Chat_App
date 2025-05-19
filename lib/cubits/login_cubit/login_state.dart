30a01f716bd0b366f3c4917c9cde307f23653238
Mahmoud Abdelaziz <mahmoudabdelaziz00@gmail.com>
2025-05-19 19:37:25 +0300

    add cubit package and create state

---
+++ lib/cubits/login_cubit/login_state.dart
@@ -0,0 +1,9 @@
+part of 'login_cubit.dart';
+
+@immutable
+sealed class LoginState {}
+
+final class LoginInitial extends LoginState {}
+final class LoginLoaded extends LoginState {}
+final class LoginSuccess extends LoginState {}
+final class LoginFailure extends LoginState {}
